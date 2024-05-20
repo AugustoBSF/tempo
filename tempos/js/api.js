56be1ef6996489277c6b66d650bc04d7107ac36d
AugustoBSF <143547534+AugustoBSF@users.noreply.github.com>
2024-05-20 13:42:08 -0300

    Add files via upload

--- tempos/js/api.js
+++ tempos/js/api.js
@@ -3,21 +3,24 @@
 const entrada_cidade = document.querySelector('.input-cidade')
 const botao = document.querySelector('.botao-busca')
 const nome_cidade = document.querySelector('.cidade')
-const temperatura = document.querySelector('.tempo')
+const temperatura = document.querySelector('.temp')
 const previsao = document.querySelector('.texto-previsao')
 const key = '0ad966783cfb2b680648d8a8e419cc40'
+const imagem = document.querySelector('#imagem')
 
 botao.addEventListener('click',buscaCidade)
 
 async function buscaCidade(){
 
-    cidade = entrada_cidade.value
+    city = entrada_cidade.value
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q= ${city}&appid=${key}&lang=pt_br&units=metric`)
     .then(resposta=>resposta.json())
 
     console.log(dados)
 
-
-
+    nome_cidade.textContent = `Tempo em ${dados.name}`
+    temperatura.textContent = `${dados.main.temp.toFixed(0)}°C`
+    previsao.textContent= dados.weather[0].description
+    imagem.src = `http://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
